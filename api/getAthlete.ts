40d6598cd90d6db1a91aacbbd79df2fcf60ef5f7
Rob Sargant <robert@sargant.com>
2022-04-27 15:25:14 +0100

    logging

--- api/getAthlete.ts
+++ api/getAthlete.ts
@@ -5,34 +5,24 @@ import fetch from 'node-fetch'
 const listener: RequestListener = async (request, response) => {
   const parameters = new URL(request.url ?? '', `https://${request?.headers.host ?? 'example.com'}`).searchParams
 
-  console.log(request.headers['x-forwarded-for'])
-
-  let athleteId = parameters.get('aid')?.toUpperCase() ?? ''
+  const athleteId = parameters.get('aid')?.toUpperCase() ?? ''
 
   if (/^$|^A[1-9]?$|^A[1-9][0-9]{0,8}$/.test(athleteId) === false) {
     return response.writeHead(400).end('Invalid Athlete ID')
   }
 
-  athleteId = athleteId.substring(1)
-
-  const headers = {
-    'user-agent': request.headers['user-agent'] ?? 'Mozilla/5.0',
-    'X-Forwarded-For': request.socket.remoteAddress ?? '::0',
-    accept: 'text/html,application/xhtml+xml,application/xml',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': '*'
-  }
-
-  console.log(headers)
-
-  const parkrunResponse = await fetch(`https://www.parkrun.org.uk/parkrunner/${athleteId}/`, {
+  const parkrunResponse = await fetch(`https://www.parkrun.org.uk/parkrunner/${athleteId.substring(1)}/`, {
     method: 'GET',
-    headers
+    headers: {
+      'user-agent': request.headers['user-agent'] ?? 'Mozilla/5.0',
+      'x-forwarded-for': request.headers['x-forwarded-for'] as string,
+      'x-real-ip': request.headers['x-real-ip'] as string
+    }
   })
 
   if (parkrunResponse.ok === false) {
     const output = await parkrunResponse.text()
-    console.log(output)
+    console.error(output)
     return response.writeHead(502).end('Unable to retrieve data from parkrun')
   }
 
