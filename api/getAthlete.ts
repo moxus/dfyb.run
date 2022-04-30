e4b5473da1d8f37b8d673c2a0d0c4ef9b73052ae
Rob Sargant <robert@sargant.com>
2022-04-30 13:04:16 +0100

    Try again

--- api/getAthlete.ts
+++ api/getAthlete.ts
@@ -1,6 +1,6 @@
 import { load } from 'cheerio'
 import { RequestListener } from 'http'
-import fetch from 'node-fetch'
+import fetch from 'cross-fetch'
 
 const listener: RequestListener = async (request, response) => {
   const parameters = new URL(request.url ?? '', `https://${request?.headers.host ?? 'example.com'}`).searchParams
@@ -11,23 +11,26 @@ const listener: RequestListener = async (request, response) => {
     return response.writeHead(400).end('Invalid Athlete ID')
   }
 
+  const realIP = (Array.isArray(request.headers['x-real-ip']) ? request.headers['x-real-ip'][0] : request.headers['x-real-ip']) ?? '127.0.0.1'
+
   const parkrunResponse = await fetch(`https://www.parkrun.org.uk/parkrunner/${athleteId.substring(1)}/`, {
     method: 'GET',
     headers: {
       'user-agent': request.headers['user-agent'] ?? 'Mozilla/5.0',
-      'x-forwarded-for': request.headers['x-forwarded-for'] as string,
-      'x-real-ip': request.headers['x-real-ip'] as string
+      'x-real-ip': realIP,
+      'x-forwarded-for': realIP,
+      forwarded: `by=dfyb.run, for:${realIP}`,
+      cookie: request.headers.cookie ?? ''
     }
   })
 
-  if (parkrunResponse.ok === false) {
-    const output = await parkrunResponse.text()
-    console.error(output)
-    return response.writeHead(502).end('Unable to retrieve data from parkrun')
-  }
-
   const profilePageContents = load(await parkrunResponse.text())
   const athleteName = profilePageContents('h2').text()
+
+  if (parkrunResponse.ok === false || athleteName.trim() === '') {
+    console.error(profilePageContents.text())
+    return response.writeHead(502).end('Unable to retrieve data from parkrun')
+  }
 
   response.writeHead(200, {
     'Content-Type': 'text/plain'
